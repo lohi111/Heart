bff94913a9ca135f2abfeb71f9cc18c0fcb6aa3b
Potupureddi-lohith <144579179+lohi111@users.noreply.github.com>
2025-05-06 18:34:02 +0530

    Update LoveLetter.js

--- src/Components/LoveLetter.js
+++ src/Components/LoveLetter.js
@@ -35,11 +35,10 @@ const LoveLetter = () => {
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
         My Dear Bangram 🖤 ,
-        You know nvu naku crush ani ! But crush is strating thing or step for love ! Ippudhu love anta mataladam manasata nenu emi chyalenu anduka byam tho cheptunna . nvu anta pranam , nvu lekapotha chachipoth ani cheppanu gani ninnu life long nee hand vadalakunda 
-        chusukunta raaa! neku interest ledhu  ani naku telusu but yakkado chinna hope ! One nvu ok chepta me parents taruvatha ninnu inthala
-        valla kanna annanu kani valla preminchinatu premista ! nvu ok anu chalu , life kotha ga vuntundhi , nvu ok annagana simhachalam
-          temple ki velali , ninnnu baga chusukovali , nenu oka manchi job tecchukovali , intlo oppinchi pelli chasukovali , pilallu 
-            illu antuu,,,life long premistu ninnu chustu brathikayali , na dream idi , na dream ni full chyu bangram . 
+        You know nvu naku crush ani ! But crush is strating thing or step for love ! nvu anta pranam , nvu lekapotha chachipoth ani cheppanu gani me parents taruvatha ninnu 
+        valla kanna annanu kani vallu preminchinatu premista ! nvu ok anu chalu , life kotha ga vuntundhi , nvu ok annagana simhachalam
+          velali , ninnnu baga chusukovali , nenu oka manchi job tecchukovali , intlo oppinchi pelli chasukovali , pilallu 
+            illu antuu,life long premistu ninnu chustu brathikayali,na dream idi,na dream ni full chyu bangram . 
               DISCLIMAR: Please mataladam manayaku !
         <br />
         @Lohith
